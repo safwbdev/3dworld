fb03b5e5fb1b01dba949de00b311079c558480cd
Stefaan <safwebdev@gmail.com>
2023-01-17 23:31:24 +0800

    update home page content

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,25 @@ function App() {
   return (
     <BrowserRouter>
       <div className='navMenu'>
+      <div className='navMenuItem'>
+      <h2>3D worlds | WIP</h2>
+            <p>
+              An experimental project dealing with the creation of immersive 3d worlds with the use use of threejs & aframe
+            </p>
+            <p className='ghLink'><a href="https://github.com/safwbdev/3dworld"><i class="fa-brands fa-square-github"></i> <span>GH Repo</span></a></p>
+            <hr />
+            <h2>Instructions:</h2>
+            <ul>
+            <li>Use WASD or Arrow buttons to move around</li>
+            <li>click and hold down you mouse button to adjust your view</li>
+            <li>i.e. Controls are similar to an FPS</li>
+
+            </ul>
+            
+        </div>
+      </div>
+      <div className='navMenu'>
+      
         {linkData.map((link) => (
           <div className='navMenuItem'>
             <h2>{link.label}</h2>
@@ -21,7 +40,9 @@ function App() {
 
               ))}
             </ul>
+            <div className='buttonRow'>
           <NavLink to={link.uri} className="mainLink">Explore</NavLink>
+              </div>
         </div>
         ))}
       </div>
